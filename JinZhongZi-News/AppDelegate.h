49324318a4955e5b7b716cd279bf4cd47bd6084a
ibokan <ibokan@9300.local>
2014-01-06 11:55:35 +0800

    Initial Commit

---
+++ JinZhongZi-News/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JinZhongZi-News
+//
+//  Created by ibokan on 14-1-6.
+//  Copyright (c) 2014年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JinZhongZi-NewsTests/JinZhongZi_NewsTests.m
@@ -0,0 +1,34 @@
+//
+//  JinZhongZi_NewsTests.m
+//  JinZhongZi-NewsTests
+//
+//  Created by ibokan on 14-1-6.
+//  Copyright (c) 2014年 ibokan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JinZhongZi_NewsTests : XCTestCase
+
+@end
+
+@implementation JinZhongZi_NewsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
